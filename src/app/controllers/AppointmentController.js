b27c9050f5eec056a303994556bb42ae422c57af
Neto Rissi <netorissi@hotmail.com>
2020-03-28 18:25:53 -0300

    schedule

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -3,8 +3,33 @@ import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.user_id, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            { model: File, as: 'avatar', attributes: ['name', 'path', 'url'] },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = yup.object().shape({
       provider_id: yup.number().required(),
