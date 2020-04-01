6637b472e512e80b20b4247727081e236fb84293
Neto Rissi <netorissi@hotmail.com>
2020-04-01 13:14:41 -0300

    sentry logs

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as sentry from '@sentry/node';
+import 'express-async-errors';
 
 import routes from './routes';
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +28,18 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      let errorResp = 'Internal server error';
+      if (errors.length > 1) errorResp = errors.map((it) => it.error.message);
+      else errorResp = errors.error.message;
+
+      return res.status(500).json({ error: errorResp });
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://58c32b0000544e69958b1a827d3ed74e@sentry.io/5185226',
+};
